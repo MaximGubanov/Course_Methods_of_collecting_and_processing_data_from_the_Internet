23affc028996f27c29bcd62d0591b9233962154b
MaximGubanov <m_gubanov@bk.ru>
2022-03-20 18:50:22 +0300

    lesson_6

---
+++ jobparser/spiders/sjobru.py
@@ -0,0 +1,25 @@
+import scrapy
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
+
+
+class SjobruSpider(scrapy.Spider):
+    name = 'sjobru'
+    allowed_domains = ['superjob.ru']
+    start_urls = ['https://russia.superjob.ru/vacancy/search/?keywords=Python']
+
+    def parse(self, response: HtmlResponse):
+        next_page = response.xpath("//a[contains(@class, 'f-test-link-Dalshe')]/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+        links = response.xpath("//a[contains(@class, '_6AfZ9 f-test-link')]/@href").getall()
+
+        for link in links:
+            yield response.follow(link, callback=self.vacancy_parse)
+
+    def vacancy_parse(self, response: HtmlResponse):
+        print()
+        name_value = response.css('h1::text').get()
+        salary_value = response.xpath("//span[@class='_1OuF_ ZON4b']//text()").getall()
+        url_value = response.url
+        yield JobparserItem(name=name_value, salary=salary_value, url=url_value)
